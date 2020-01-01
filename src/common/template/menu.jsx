ed1388e1ce414307d4ed63897da556b2d735ef1e
Nilsiitcho <nyson96@hotmail.com>
2019-12-31 21:26:20 -0300

    Mudando iconografia

--- src/common/template/menu.jsx
+++ src/common/template/menu.jsx
@@ -6,22 +6,22 @@ export default props => (
     <ul className="sidebar-menu">
         <MenuItem path="/" label="Home" icon="home"/>
 
-        <MenuTree label="Loja" icon="connectdevelop">
+        <MenuTree label="Loja" icon="shopping-cart">
             <MenuItem path="/loja/produtos" label="Produtos" icon="usd"/>
             <MenuItem path="/loja/meus-pedidos" label="Meus pedidos" icon="usd"/>
         </MenuTree>
 
         <MenuTree label="Rede" icon="connectdevelop">
             <MenuItem path="/rede/unilevel" label="Unilevel" icon="usd"/>
-            <MenuItem path="/rede/direto" label="Direto" icon="usd"/>
+            <MenuItem path="/rede/direto" label="Direto" icon="sitemap"/>
         </MenuTree>
 
-        <MenuTree label="Financeiro" icon="connectdevelop">
+        <MenuTree label="Financeiro" icon="usd">
             <MenuItem path="/rede/historico" label="Histórico" icon="usd"/>
-            <MenuItem path="/rede/retiradas" label="Retiradas" icon="usd"/>
+            <MenuItem path="/rede/retiradas" label="Retiradas" icon="money"/>
         </MenuTree>
 
-        <MenuItem path="/suporte" label="Suporte" icon="usd"/>
-        <MenuItem path="/contrato" label="Contrato" icon="usd"/>
+        <MenuItem path="/suporte" label="Suporte" icon="phone"/>
+        <MenuItem path="/contrato" label="Contrato" icon="file-text-o"/>
     </ul>
 )
