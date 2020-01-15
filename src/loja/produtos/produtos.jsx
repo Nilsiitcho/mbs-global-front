b3850c5d2967a4aa86b0fcc497a8e83d7ecdb1cf
Nilsiitcho <nyson96@hotmail.com>
2020-01-14 22:08:32 -0300

    Começando a criar component Produtos

---
+++ src/loja/produtos/produtos.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Content from "../../common/template/content";
+import ContentHeader from "../../common/template/contentHeader";
+
+
+export default props => (
+    <div>
+        <ContentHeader title="Produtos"/>
+        <Content>
+            <p>eta nois hehe</p>
+        </Content>
+    </div>
+)
--- src/main/routes.jsx
+++ src/main/routes.jsx
@@ -7,12 +7,14 @@ import Diretos from "../rede/diretos/diretos"
 import Retiradas from "../financeiro/retiradas/retiradas"
 import Historico from "../financeiro/historico/historico";
 import MeusPedidos from "../loja/meusPedidos/meusPedidos";
+import Produtos from "../loja/produtos/produtos"
 
 export default props => (
     <div className="content-wrapper">
         <Switch>
             <Route exact path="/" component={Home}/>
             <Route path="/loja/meusPedidos" component={MeusPedidos}/>
+            <Route path="/loja/produtos" component={Produtos}/>
             <Route path="/rede/diretos" component={Diretos}/>
             <Route path="/financeiro/retiradas" component={Retiradas}/>
             <Route path="/financeiro/historico" component={Historico}/>
