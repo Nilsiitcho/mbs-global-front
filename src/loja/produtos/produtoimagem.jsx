d8aa96fd9857e24f3a3c14fe77cb31b0dae0a525
Nilson Neto <nyson96@hotmail.com>
2020-01-16 19:35:03 -0300

    Criando componente de Produto e imagemProduto

---
+++ src/loja/produtos/produtoimagem.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import Grid from "../../common/layout/grid";
+
+export default props => (
+    <Grid cols='12 3'>
+        <div className="produto-container">
+            <h3>{props.title}</h3>
+            <img src={props.src} className="produto-image" alt="imagem do produto"/>
+            <div className="produto-decsricao">
+                <strong>{`Valor: ${props.valor}`}</strong>
+                <br/>
+                <strong>{`Bônus de rede: ${props.bonus}`}</strong>
+            </div>
+            <button className="btn btn-primary" onClick={props.onClick}><i className={'fa fa-shopping-cart'}/> Comprar
+            </button>
+        </div>
+    </Grid>
+)
--- src/loja/produtos/produtos.jsx
+++ src/loja/produtos/produtos.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Content from "../../common/template/content";
 import ContentHeader from "../../common/template/contentHeader";
+import ProdutoImagem from "./produtoimagem";
 
 
 export default props => (
     <div>
         <ContentHeader title="Produtos"/>
         <Content>
-            <p>eta nois hehe</p>
+            <ProdutoImagem src="https://www.vagalume.com.br/lana-del-rey/images/lana-del-rey.jpg" title="Laninha"
+                           valor="R$300,00" bonus="R$100,00" onClick={() => alert('comprei')}/>
         </Content>
     </div>
 )
