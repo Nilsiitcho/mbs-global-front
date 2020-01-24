d28d291165fd23858b5f166b34c86fae9fc33017
Nilson Neto <nyson96@hotmail.com>
2020-01-24 03:06:33 -0300

    Adicionando carregamento à tela de produtos

--- src/loja/produtos/produtos.jsx
+++ src/loja/produtos/produtos.jsx
@@ -5,20 +5,24 @@ import ProdutoImagem from "./produtoimagem";
 import Row from "../../common/layout/row";
 import Grid from "../../common/layout/grid";
 import Pages from "../../common/template/paginacao";
+import Loading from "../../common/template/loading";
 import axios from "axios";
 
 export default () => {
 
+    const [isLoading, setIsLoading] = useState(true);
     const [produtos, setProdutos] = useState([]);
     const [totalPaginas, setTotalPaginas] = useState(1);
     const [paginaAtual, setPaginaAtual] = useState(1);
 
     async function getData(page = 1) {
-        const {data} = await axios.get(`http://localhost:3001/produto?limit=5&skip=${page}`);
+        setIsLoading(true);
+        const {data} = await axios.get(`http://localhost:3001/produto?limit=12&skip=${page}`);
         const pages = data.pages;
         setTotalPaginas(pages);
         setPaginaAtual(page);
         setProdutos(data.docs);
+        setIsLoading(false);
     }
 
     useEffect(() => {
@@ -34,25 +38,41 @@ export default () => {
         ))
     }
 
+    function renderDataOrLoading(){
+        if(isLoading){
+            return(
+                <div className="loading-cadastro-produtos">
+                    <Loading height={100} width={100}/>
+                </div>
+            )
+        }else{
+            return (
+                <Fragment>
+                    <Row>
+                        <Grid cols="12 6">
+                            <Pages totalDePaginas={totalPaginas} paginaAtual={paginaAtual}
+                                   callBack={(data) => getData(data)}/>
+                        </Grid>
+                    </Row>
+                    <Row>
+                        {renderProduto()}
+                    </Row>
+                    <Row>
+                        <Grid cols="12 6">
+                            <Pages totalDePaginas={totalPaginas} paginaAtual={paginaAtual}
+                                   callBack={(data) => getData(data)}/>
+                        </Grid>
+                    </Row>
+                </Fragment>
+            )
+        }
+    }
+
     return (
         <Fragment>
             <ContentHeader title="Produtos"/>
             <Content>
-                <Row>
-                    <Grid cols="12 6">
-                        <Pages totalDePaginas={totalPaginas} paginaAtual={paginaAtual}
-                               callBack={(data) => getData(data)}/>
-                    </Grid>
-                </Row>
-                <Row>
-                    {renderProduto()}
-                </Row>
-                <Row>
-                    <Grid cols="12 6">
-                        <Pages totalDePaginas={totalPaginas} paginaAtual={paginaAtual}
-                               callBack={(data) => getData(data)}/>
-                    </Grid>
-                </Row>
+                {renderDataOrLoading()}
             </Content>
         </Fragment>
     )
